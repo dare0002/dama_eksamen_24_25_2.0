31a5265d7cb67d006f5d4922b230c2e137043d6b
Daniela <dare0002@stud.kea.dk>
2024-12-05 15:18:28 +0100

    Button CTA first try done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,7 @@
 const Button = () => {
-    return (  );
+    return ( 
+        <button className="border border-offwhite text-offwhite bg-transparent py-2 px-6 text-lg hover:bg-pink hidden md:block"></button>
+     );
 }
  
 export default Button;

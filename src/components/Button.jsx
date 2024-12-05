cca5a541cd9ccff3d354684c76deafbb63bf750f
Daniela <dare0002@stud.kea.dk>
2024-12-05 16:06:00 +0100

    button CTA updated with porps + tailwind css updated with color: black

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-const Button = () => {
+const Button = ({ children, background }) => {
     return ( 
-        <button className="border border-offwhite text-offwhite bg-transparent py-2 px-6 text-lg hover:bg-pink hidden md:block"></button>
+        <button className={`border border-offwhite text-offwhite py-2 px-6 text-lg hover:bg-pink hidden md:block 
+            ${background ? "bg-offwhite text-black" : "bg-transparent"
+                
+            }`}
+            >
+                {children}
+        </button>
      );
 }
  

c6d5239b32e37e944cf7691f37ec48f9bb71d172
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-05 20:17:31 +0100

    :)

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <main className="">
-        <div>HEROOO</div>
+        <div> <Image></Image></div>
         <IndexBoxes />
         <Banner />
         <FAQ />

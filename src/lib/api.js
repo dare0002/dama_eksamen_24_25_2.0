a4039782d68df2f70a91630b350d5167784d037d
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-06 11:38:48 +0100

    fetching not done

---
+++ src/lib/api.js
@@ -0,0 +1,30 @@
+
+
+
+ 
+   
+//    async function apiFetch(url, options  = {}) {
+//     const response = await fetch(url, options);
+//     if (!response.ok){
+//         return await response.json()
+//     }
+//    }
+
+//    export async function getBands(){
+//     return apiFetch
+//    }
+
+
+
+//    const headersList = {
+//     "Accept": "*/*",
+//     "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+//    }
+     
+//    const response = await fetch("http://localhost:8080/bands", { 
+//     method: "GET",
+//     headers: headersList
+//   });
+  
+//   const data = await response.json();
+//   console.log(data);

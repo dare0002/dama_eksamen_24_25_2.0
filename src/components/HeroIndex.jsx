343acf128b98e6e68d626351f066e64cd9ba158c
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-13 11:07:04 +0100

    responsive done

--- src/components/HeroIndex.jsx
+++ src/components/HeroIndex.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 const HeroIndex = () => {
   return (
-    <div>
-      <div className="absolute -inset-36 text-center ">
+    <div className="w-full text-offwhite z-0 flex place-content-center">
+      <div className="absolute  text-center w-full top-6 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h1 className=" text-4xl font-bold ">FOOFEST</h1>
-        <h3 className="text-3xl">7-DAY FESTIVAL</h3>
+        <h3 className="text-2xl">7-DAY FESTIVAL</h3>
       </div>
       <Image
         src="/hero.png"

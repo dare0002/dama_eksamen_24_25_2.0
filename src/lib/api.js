1893f9fd1078da4dad641466e4efd0daa24a6d62
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-17 10:56:59 +0100

    tjek api ok

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,28 +7,20 @@ async function apiFetch(url, options = {}) {
   return response.json();
 }
 
-
-
-
-
-
-
-
 // DANIIIII CAMPING
 
-export async function getAvailableSpots() {
-  const response = await fetch("http://localhost:8080/available-spots");
-  const data = await response.json();
+// export async function getAvailableSpots() {
+//   const response = await fetch("http://localhost:8080/available-spots");
+//   const data = await response.json();
 
-  const spots = Object.keys(data).map((area) => ({
-    area, // Nyckeln (området)
-    spots: data[area].spots, // Totala platser
-    available: data[area].available, // Tillgängliga platser
-  }));
+//   const spots = Object.keys(data).map((area) => ({
+//     area, // Nyckeln (området)
+//     spots: data[area].spots, // Totala platser
+//     available: data[area].available, // Tillgängliga platser
+//   }));
 
-  return spots; // Returnerar en array av områden med tillhörande data
-}
-
+//   return spots; // Returnerar en array av områden med tillhörande data
+// }
 
 // export async function getAvailableSpots () {
 //   const response = await fetch ("http://localhost:8080/available-spots");
@@ -38,57 +30,48 @@ export async function getAvailableSpots() {
 
 //   Object.keys(data).forEach((spots) => {
 //     Object.keys(data[spots]).forEach((available) => {
-//       spots.push(...data[spots][available]); 
+//       spots.push(...data[spots][available]);
 //     });
 //   });
 
 //   return spots;
 // }
 
-// Reserve spot 
+// Reserve spot
 
-export async function reserveSpot (area, amount) {
-  const response = await fetch("http://localhost:8080/reserve-spot", {
-    method: "PUT", 
-    headers: {
-      "Content-Type" : "application/json", 
-    }, 
-    body: JSON.stringify({area, amount}),
-  });
+// export async function reserveSpot (area, amount) {
+//   const response = await fetch("http://localhost:8080/reserve-spot", {
+//     method: "PUT",
+//     headers: {
+//       "Content-Type" : "application/json",
+//     },
+//     body: JSON.stringify({area, amount}),
+//   });
 
-  if (!response.ok) {
-    throw new Error("Failed to reserve spot");
-  }
+//   if (!response.ok) {
+//     throw new Error("Failed to reserve spot");
+//   }
 
-  // Returnerar ID för den specifika bokningen
-  const data = await response.json();
-  return data.id;
-}
+//   // Returnerar ID för den specifika bokningen
+//   const data = await response.json();
+//   return data.id;
+// }
 
-export async function fulfillReservation(id) {
-  const response = await fetch("http://localhost:8080/fullfill-reservation", {
-    method: "POST", 
-    headers: {
-      "Content-Type" : "application/json", 
-    }, 
-    body: JSON.stringify({id}),
-  });
+// export async function fulfillReservation(id) {
+//   const response = await fetch("http://localhost:8080/fullfill-reservation", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type" : "application/json",
+//     },
+//     body: JSON.stringify({id}),
+//   });
 
-  if (!response.ok) {
-    throw new Error("Failed to fulfill reservation");
-  }
+//   if (!response.ok) {
+//     throw new Error("Failed to fulfill reservation");
+//   }
 
-  return await response.json()
-}
-
-
-
-
-
-
-
-
-
+//   return await response.json()
+// }
 
 export async function getData() {
   return apiFetch(`${endpoint}?order=id.desc`, {

cebd5ce4b3b632c40e6e0d3c8c9d3afa1a50d99b
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-17 14:34:20 +0100

    :)

--- src/app/Artist/[slug]/page.js
+++ src/app/Artist/[slug]/page.js
@@ -1,30 +1,3 @@
-// const Artist = async ({ params }) => {
-//   const { slug } = await params;
-
-//   try {
-//     const response = await fetch(`http://localhost:8080/bands/${slug}`);
-
-//     if (!response.ok) {
-//       console.error(`HTTP error! Status: ${response.status}`);
-//       return <div>Error: Band not found or server error.</div>;
-//     }
-
-//     const rawText = await response.text(); // Fetch raw text
-//     console.log("Raw API Response:", rawText); // Debug response
-
-//     if (!rawText) {
-//       throw new Error("Empty response from server.");
-//     }
-
-//     const band = JSON.parse(rawText); // Parse JSON safely
-//     return <ArtistCard bands={[band]} />;
-//   } catch (error) {
-//     console.error("Error during fetch:", error.message);
-//     return <div>Error: Failed to load artist data.</div>;
-//   }
-// };
-
-// export default Artist;
 import ArtistCard from "./components/ArtistCard";
 
 const Artist = async ({ params }) => {

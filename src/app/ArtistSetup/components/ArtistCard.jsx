a18ad2748b76736c309be72b00c129e14ac97287
Marie Wulff <mawu0001@stud.kea.dk>
2024-12-18 10:49:12 +0100

    test deployemtn

--- src/app/ArtistSetup/components/ArtistCard.jsx
+++ src/app/ArtistSetup/components/ArtistCard.jsx
@@ -1,55 +1,55 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-const ArtistCard = ({ bands }) => {
-  return (
-    <div>
-      {bands.map((band, index) => (
-        <section
-          key={index}
-          className="grid grid-cols-6 border border-solid mx-20"
-        >
-          <div className="col-start-1 col-span-2 border border-solid border-offwhite">
-            <h2>{band.name}</h2>
-          </div>
-          <div className="col-start-3 col-span-3 row-start-1 row-span-3 border border-solid border-offwhite">
-            <Image src={""} width={400} height={400} alt={band.name} />
-          </div>
-          <div className="col-start-1 col-span-2 border border-solid border-offwhite">
-            <p>{band.bio}</p>
-          </div>
-          <div className="row-start-4 border border-solid border-offwhite">
-            <h3>Members.</h3>
-            <h4>
-              {band.members.map((member, i) => (
-                <span key={i}>{member}</span>
-              ))}
-            </h4>
-          </div>
-          <div className="col-start-3 col-span-1 border border-solid border-offwhite">
-            <h4>DATE.</h4>
-          </div>
-          <div className="border border-solid">
-            <h4>{band.date}</h4>
-          </div>
-          <div className="col-start-3 col-span-1 border border-solid border-offwhite">
-            <h4>STAGE.</h4>
-          </div>
-          <div className="border border-solid border-offwhite">
-            <h4>{band.stage}</h4>
-          </div>
-          <div className="border border-solid col-start-1 border-offwhite">
-            <h4>GENRE.</h4>
-          </div>
-          <div className="col-start-2 col-span-2 border border-solid border-offwhite">
-            <h4>{band.genre}</h4>
-          </div>
-          <div className="bg-orange col-start-3 col-span-2 row-start-6 row-span-2 border border-solid border-offwhite">
-            <button>BUY TICKETS.</button>
-          </div>
-        </section>
-      ))}
-    </div>
-  );
-};
+// const ArtistCard = ({ bands }) => {
+//   return (
+//     <div>
+//       {bands.map((band, index) => (
+//         <section
+//           key={index}
+//           className="grid grid-cols-6 border border-solid mx-20"
+//         >
+//           <div className="col-start-1 col-span-2 border border-solid border-offwhite">
+//             <h2>{band.name}</h2>
+//           </div>
+//           <div className="col-start-3 col-span-3 row-start-1 row-span-3 border border-solid border-offwhite">
+//             <Image src={""} width={400} height={400} alt={band.name} />
+//           </div>
+//           <div className="col-start-1 col-span-2 border border-solid border-offwhite">
+//             <p>{band.bio}</p>
+//           </div>
+//           <div className="row-start-4 border border-solid border-offwhite">
+//             <h3>Members.</h3>
+//             <h4>
+//               {band.members.map((member, i) => (
+//                 <span key={i}>{member}</span>
+//               ))}
+//             </h4>
+//           </div>
+//           <div className="col-start-3 col-span-1 border border-solid border-offwhite">
+//             <h4>DATE.</h4>
+//           </div>
+//           <div className="border border-solid">
+//             <h4>{band.date}</h4>
+//           </div>
+//           <div className="col-start-3 col-span-1 border border-solid border-offwhite">
+//             <h4>STAGE.</h4>
+//           </div>
+//           <div className="border border-solid border-offwhite">
+//             <h4>{band.stage}</h4>
+//           </div>
+//           <div className="border border-solid col-start-1 border-offwhite">
+//             <h4>GENRE.</h4>
+//           </div>
+//           <div className="col-start-2 col-span-2 border border-solid border-offwhite">
+//             <h4>{band.genre}</h4>
+//           </div>
+//           <div className="bg-orange col-start-3 col-span-2 row-start-6 row-span-2 border border-solid border-offwhite">
+//             <button>BUY TICKETS.</button>
+//           </div>
+//         </section>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default ArtistCard;
+// export default ArtistCard;
--- src/app/ArtistSetup/page.js
+++ src/app/ArtistSetup/page.js
@@ -1,13 +1,13 @@
-import ArtistCard from "./components/ArtistCard";
+// import ArtistCard from "./components/ArtistCard";
 
-const Artist = async () => {
-  // const response = await fetch("https://localhost:8080/bands");
-  // const bands = await response.json();
+// const Artist = async () => {
+//   // const response = await fetch("https://localhost:8080/bands");
+//   // const bands = await response.json();
 
-  const response = await fetch("https://kindly-elegant-fork.glitch.me/bands");
-  const bands = await response.json();
+//   const response = await fetch("https://kindly-elegant-fork.glitch.me/bands");
+//   const bands = await response.json();
 
-  return <ArtistCard bands={bands} />;
-};
+//   return <ArtistCard bands={bands} />;
+// };
 
-export default Artist;
+// export default Artist;
